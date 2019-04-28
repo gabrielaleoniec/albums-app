d13ba1e9f394c9f77e1be89f3d7f5819dab17992
Gaba <gabriela.leoniec@gmail.com>
2019-04-28 23:13:46 +0200

    Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+const AlbumsReducer = () => {
+    return [
+        {
+            band: 'Beatles',
+            album: 'Yellow Submarine',
+            displaySongs: false,
+            songs: ['Yellow Submarine', 'Only a Northern Song', 'Hey Bulldog']
+        }
+    ];
+
+}
