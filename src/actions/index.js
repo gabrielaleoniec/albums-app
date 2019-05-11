534b74c78bc8a7c950b828282fb451c99b88e313
Gaba <gabriela.leoniec@gmail.com>
2019-05-11 21:32:30 +0200

    Refactoring the code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,14 @@
 import albumsConnection from "../apis/albums";
-import _ from "lodash";
 
 export const fetchAlbumsAndSongs = () => async dispatch => {
   const response = await albumsConnection.get("/data.json");
   let albumsComplete = {};
   for (const { band, album, song } of response.data) {
-    let key = _.snakeCase(band + album);
+    let key = (band+album).replace(/[^a-z0-9]/ig, "");
 
-    if (albumsComplete[key]) {
-      let songs = albumsComplete[key].songs;
-      if (Array.isArray(songs) && songs.indexOf(song) === -1) {
-        songs.push(song);
+    if (albumsComplete[key]  && Array.isArray(albumsComplete[key].songs)) {
+      if (albumsComplete[key].songs.indexOf(song) === -1) {
+        const songs = [...albumsComplete[key].songs, song];
         albumsComplete[key] = { band, album, songs };
       }
     } else {
@@ -32,7 +30,7 @@ export const displaySongs = (displaySongs = {}, albumId = 0) => dispatch => {
     type: "DISPLAY_SONGS",
     payload: {
       ...displaySongs,
-      [albumId]: !displaySongs[albumId],
+      [albumId]: !displaySongs[albumId]
     }
   });
 };
