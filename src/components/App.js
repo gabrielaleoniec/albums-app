d2ead5aea831fad07ea41aae0b875725b75d3071
Gabriela Leoniec <gabriela.leoniec@gmail.com>
2019-05-12 22:44:40 +0200

    Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import AlbumList from "./AlbumList";
+import "./App.scss";
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <AlbumList />
+      <div className="app">
+        <AlbumList className="albums"/>
+        <a href="https://www.freepik.com/free-photos-vectors/background" className="footer__bottom">Background vector created by starline - www.freepik.com</a>
       </div>
     );
   }
